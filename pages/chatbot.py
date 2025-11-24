a3d31a663f9cc1a563c9e75ad8a0c343853bdc38
Er <ercj@ncbi.nlm.nih.gov>
2025-11-24 17:08:57 -0600

    edited chatbot text

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -197,7 +197,7 @@ def run_chatbot(prolific_id: str):
     col1, col2 = st.columns([1, 1], gap="large")
     with col1:
         st.markdown(f"### ABSTRACT")
-        btn_col1, btn_col2, btn_col3 = st.columns([0.25, 0.5, 0.25])
+        btn_col1, btn_col2, btn_col3 = st.columns([0.25, 0.65, 0.10])
         with btn_col1:
             if st.button("Decrease text size"):
                 st.session_state.abstract_font_size = max(12, st.session_state.abstract_font_size - 2)
