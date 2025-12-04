0cc3a887c9ca17b37b898d5dcf9265e10fa8d72d
Er <ercj@ncbi.nlm.nih.gov>
2025-12-03 20:11:15 -0600

    fixed

--- pages/term_familarity_page.py
+++ pages/term_familarity_page.py
@@ -111,7 +111,6 @@ def abstract_fragment(abs_item, font_size):
         """,
         unsafe_allow_html=True
     )
-@st.fragment
 def familiarity_page(abs_item, abstract_id):
     st.subheader("How familiar are you with each term?")
     st.markdown("""
