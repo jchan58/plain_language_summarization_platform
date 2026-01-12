8bd6ceff3dd057ddc04aa69bc2dbd99b9e32c229
Er <ercj@ncbi.nlm.nih.gov>
2026-01-12 17:31:14 -0600

    fixed

--- app.py
+++ app.py
@@ -131,11 +131,7 @@ if not st.session_state.get("logged_in", False):
                 else:
                     structured_terms = []
                 
-                if pd.isna(row["abstract_id"]):
-                    print("FOUND NaN abstract_id for user:", prolific_id)
-                    print(row)
                 abstract_key = str(row["abstract_id"])
-
                 phases[phase_type]["batches"][batch_id]["abstracts"][abstract_key]= {
                     "abstract_title": row["abstract_title"],
                     "abstract": re.sub(r"\s+", " ", ftfy.fix_text(row["abstract"])).strip(),
