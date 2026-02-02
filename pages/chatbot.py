64bb0d8b62b466198085afd988356634d08bbacc
Er <ercj@ncbi.nlm.nih.gov>
2026-02-02 14:58:50 -0600

    edited the instructions

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -135,21 +135,18 @@ def interactive_instructions(prolific_id, batch_id):
      For this interactive phase, you will complete **1 abstract**. For the abstract, you will be asked to do the following.
 
     1. Use the AI chatbot to ask any questions you have to help you better understand the ABSTRACT and any questions you may have about it:\n
-       **For example:**\n
-        “Is this medication FDA approved for me to take?”
+       &nbsp;&nbsp;For example:
+       &nbsp;&nbsp;“Is this medication FDA approved for me to take?”
     2. You will then be shown an AI-Generated SUMMARY derived from the ABSTRACT. Read the AI-Generated SUMMARY and answer all five Select-All-That-Apply (SATA) questions using the information provided in the AI-Generated SUMMARY.
     3. You will then be shown both the ABSTRACT and the AI-Generated SUMMARY and asked to complete the following:\n
-        (a) Comparing the AI-Generated SUMMARY to the ABSTRACT:
-                
-        Answer questions that evaluate how the AI-Generated SUMMARY compares to the ABSTRACT in terms of clarity, organization, coverage of information, inclusion of background information, and trustworthiness.
+        &nbsp;&nbsp;(a) Comparing the AI-Generated SUMMARY to the ABSTRACT:
+        &nbsp;&nbsp;Answer questions that evaluate how the AI-Generated SUMMARY compares to the ABSTRACT in terms of clarity, organization, coverage of information, inclusion of background information, and trustworthiness.
 
-        (b) Thinking only about the AI-Generated SUMMARY:
-                
-        Answer questions that assess the AI-Generated SUMMARY on its own, including whether it met your information needs.
+        &nbsp;&nbsp;(b) Thinking only about the AI-Generated SUMMARY: 
+        &nbsp;&nbsp;Answer questions that assess the AI-Generated SUMMARY on its own, including whether it met your information needs.
 
-        (c) Your experience using the AI chatbot:
-                
-        Answer a few questions about your experience using the AI chatbot in this study.
+        &nbsp;&nbsp;(c) Your experience using the AI chatbot:
+        &nbsp;&nbsp;Answer a few questions about your experience using the AI chatbot in this study.
     ---
     **Additional Notes:**
     - Refer to the instructions at the top of each page for detailed guidance.  
